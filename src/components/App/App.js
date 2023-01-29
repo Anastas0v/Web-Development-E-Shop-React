a2091ed46b28f7dfcabb67877826fac7dfd29e39
Anastas0v <anastasovdavid@gmail.com>
2023-01-29 01:31:43 +0100

    implemented axios and get method for manufacturers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import {Component} from "react";
+import Manufacturers from "../Manufacturers/manufacturers";
+import EShopService from "../../repository/eShopRepository";
 
-function App() {
-  return (
-    <div>
-      <h1>E-Shop Application</h1>
-    </div>
-  );
+class App extends Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            manufacturers: []
+        }
+    }
+
+    loadManufacturers = () => {
+        EShopService.fetchManufacturers()
+            .then((data) => {
+                this.setState({
+                    manufacturers: data.data
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.loadManufacturers();
+    }
+
+    render() {
+        return (
+            <div>
+                <Manufacturers manufacturers={this.state.manufacturers}/>
+            </div>
+        )
+    }
 }
 
 export default App;
